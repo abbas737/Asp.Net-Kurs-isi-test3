97ba17163f075a60fbb1b871c31db2defa1994b4
Abbas <abbasesgerli68@gmail.com>
2026-05-18 22:58:29 +0400

    created

--- obj/Debug/net8.0/Tank Wiki.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tank Wiki.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tank Wiki")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f7f64405620b169d1c89a894c9181dd3d402e0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6281c5f1330588f668a58bde09b0ffa4a2d8a41a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tank Wiki")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tank Wiki")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
